de857401ab3f29e8b3f61664c2ef75f20d693d48
cifarquhar <colin.farquhar@codeclan.com>
2018-10-02 11:25:45 +0100

    add GET and POST routes to controller

--- server/src/main/java/com/codeclan/example/beer_recipe_api/controller/BeerController.java
+++ server/src/main/java/com/codeclan/example/beer_recipe_api/controller/BeerController.java
@@ -1,4 +1,31 @@
 package com.codeclan.example.beer_recipe_api.controller;
 
+import com.codeclan.example.beer_recipe_api.model.Beer;
+import com.codeclan.example.beer_recipe_api.repository.BeerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
 public class BeerController {
+
+    @Autowired
+    private BeerRepository beerRepository;
+
+    @GetMapping("/beers")
+    public Page<Beer> getBeers(Pageable pageable){
+        return beerRepository.findAll(pageable);
+    }
+
+    @PostMapping("/beers")
+    public Beer createBeer(@Valid @RequestBody Beer beer){
+        return beerRepository.save(beer);
+    }
+
 }
