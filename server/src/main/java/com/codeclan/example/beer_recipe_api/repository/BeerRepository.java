6c9b44d28e3dda88367f3147e41857f21a9cfb5f
cifarquhar <colin.farquhar@codeclan.com>
2018-10-02 11:21:12 +0100

    add repository

--- server/src/main/java/com/codeclan/example/beer_recipe_api/repository/BeerRepository.java
+++ server/src/main/java/com/codeclan/example/beer_recipe_api/repository/BeerRepository.java
@@ -1,4 +1,9 @@
 package com.codeclan.example.beer_recipe_api.repository;
 
-public interface BeerRepository {
+import com.codeclan.example.beer_recipe_api.model.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BeerRepository extends JpaRepository<Beer, Long> {
 }
