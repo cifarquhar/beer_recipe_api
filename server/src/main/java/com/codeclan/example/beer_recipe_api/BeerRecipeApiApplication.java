8b766d4b60866fd629be1f91f418aa2acbf32f39
cifarquhar <colin.farquhar@codeclan.com>
2018-10-02 10:57:41 +0100

    add JPA auditing annotation

--- server/src/main/java/com/codeclan/example/beer_recipe_api/BeerRecipeApiApplication.java
+++ server/src/main/java/com/codeclan/example/beer_recipe_api/BeerRecipeApiApplication.java
@@ -2,8 +2,10 @@ package com.codeclan.example.beer_recipe_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BeerRecipeApiApplication {
 
 	public static void main(String[] args) {
