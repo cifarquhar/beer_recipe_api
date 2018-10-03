1eee720e9e0dfc3cb5262b5674d26496221ee87d
cifarquhar <colin.farquhar@codeclan.com>
2018-10-03 16:37:10 +0100

    add form to Add component

--- client/src/components/Add.js
+++ client/src/components/Add.js
@@ -2,10 +2,46 @@ import React from "react";
 
 class Add extends React.Component{
 
+  constructor(props){
+    super(props)
+    this.state = {
+      name: "",
+      style: "",
+      description: ""
+    }
+    this.handleNameChange = this.handleNameChange.bind(this)
+    this.handleStyleChange = this.handleStyleChange.bind(this)
+    this.handleDescriptionChange = this.handleDescriptionChange.bind(this)
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    console.log(this.state);
+    this.setState({name: "", style: "", description: ""});
+  }
+
+  handleNameChange(event){
+    this.setState({name: event.target.value})
+  }
+
+  handleStyleChange(event) {
+    this.setState({ style: event.target.value })
+  }
+
+  handleDescriptionChange(event) {
+    this.setState({ description: event.target.value })
+  }
+
   render(){
     return(
-      <div className="new-recipe-div">
+      <div className="new-recipe-div" onSubmit={this.handleSubmit.bind(this)}>
         <p>Form to add a new recipe - work in progress</p>
+        <form>
+          <input type="text" placeholder="name" value={this.state.name} onChange={this.handleNameChange}/>
+          <input type="text" placeholder="style" value={this.state.style} onChange={this.handleStyleChange}/>
+          <textarea type="text" placeholder="description" value={this.state.description} onChange={this.handleDescriptionChange}/>
+          <input type="submit" value="Add Recipe"/>
+        </form>
       </div>
     )
   }
