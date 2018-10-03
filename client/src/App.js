36551cfda63e7370730f18f47a546e9df4cf586f
cifarquhar <colin.farquhar@codeclan.com>
2018-10-03 14:49:41 +0100

    add NavBar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
 import BeerList from "./components/BeerList";
+import NavBar from './components/NavBar';
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <BeerList />
-      </div>
+
+      <Router>
+
+        <div className="App">
+          <NavBar />
+
+
+          <BeerList />
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => (
+  <ul>
+    <li>
+      <Link to="/">Home</Link>
+    </li>
+    <li>
+      <Link to="/favourites">Favourites</Link>
+    </li>
+    <li>
+      <Link to="/new">Add New Recipe</Link>
+    </li>
+  </ul>
+);
+
+export default Navbar;
